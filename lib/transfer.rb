c490120d820b6f73e49b6d2adb573363ddebb00b
Romario Gustave <romagustave10@gmail.com>
2020-05-10 02:15:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,8 +18,8 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    elsif
-      @status = "rejected"
+    #elsif
+      #@status = "rejected"
       return "Transaction rejected. Please check your account balance."
     else
       return "Transaction rejected. Please check your account balance."
